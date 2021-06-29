281c2b5e1ef10846ea54e5a7d9d152159ab9b502
singyeongdeog <tlsrudejr123@nate.com>
2021-06-30 00:36:11 +0900

    [ADD] (spring_basic3) 스프링 컨테이너 생성

---
+++ spring_basic3_create_spring_container/core/src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,32 @@
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
+    }
+    @Bean
+    public MemberRepository memberRepository() { // memberRepository는 memory로 쓸것이다 .라는 것을 여기애서 정의.
+        return new MemoryMemberRepository();
+    }
+    @Bean
+    public OrderService orderService() { // orderService는 정의되어있는 member, discount 객체를 가져옴 ( orderService가 스스로 정의하는것이 아님)
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+    @Bean
+    public DiscountPolicy discountPolicy() { // disCount정책에 관련해서는 여기에서 정의
+        return new RateDiscountPolicy();
+    }
+}
