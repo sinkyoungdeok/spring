112b73ea86f1e1c445cf7f36dded67b90e9c0e19
singyeongdeog <tlsrudejr123@nate.com>
2021-07-01 23:32:58 +0900

    [ADD] (spring_basic4) 롬복과 최신 트랜드

--- spring_basic4_component_scan/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ spring_basic4_component_scan/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,10 +3,11 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
@@ -14,11 +15,6 @@ public class OrderServiceImpl implements OrderService {
     //private DiscountPolicy discountPolicy = new RateDiscountPolicy();
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
