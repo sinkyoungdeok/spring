b5d0501d104b8591a77513d000332304f8e188b6
sinkyoungdeok <tlsrudejr123@nate.com>
2021-07-04 14:44:06 +0900

    [ADD] (spring_basic5) 조회한 빈이 모두 필요 할 때, List, Map

--- spring_basic5_autowired/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ spring_basic5_autowired/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,7 +6,6 @@ import hello.core.member.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
