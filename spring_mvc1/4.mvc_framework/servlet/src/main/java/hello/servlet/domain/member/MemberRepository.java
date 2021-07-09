516f90b87e2e174c999b19fa2426150c92e42c65
sinkyoungdeok <tlsrudejr123@nate.com>
2021-07-09 17:07:01 +0900

    [ADD] (spring_mvc1-4) Model 추가 - v3

--- spring_mvc1/4.mvc_framework/servlet/src/main/java/hello/servlet/domain/member/MemberRepository.java
+++ spring_mvc1/4.mvc_framework/servlet/src/main/java/hello/servlet/domain/member/MemberRepository.java
@@ -22,7 +22,7 @@ public class MemberRepository {
     private MemberRepository() {
     }
 
-    public Member save(Member member) {
+    public static Member save(Member member) {
         member.setId(++sequence);
         store.put(member.getId(), member);
         return member;
