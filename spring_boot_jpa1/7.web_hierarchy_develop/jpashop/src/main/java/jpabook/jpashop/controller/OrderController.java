09f8847a6513876651e8b3cffbb867bcb361d877
sinkyoungdeok <tlsrudejr123@nate.com>
2021-09-19 16:11:39 +0900

    [ADD] (웹 계층 개발) 주문 도메인 개발 관련된 코드가 사라져서 추가

--- spring_boot_jpa1/7.web_hierarchy_develop/jpashop/src/main/java/jpabook/jpashop/controller/OrderController.java
+++ spring_boot_jpa1/7.web_hierarchy_develop/jpashop/src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -1,4 +1,11 @@
 package jpabook.jpashop.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
 public class OrderController {
+
+
 }
