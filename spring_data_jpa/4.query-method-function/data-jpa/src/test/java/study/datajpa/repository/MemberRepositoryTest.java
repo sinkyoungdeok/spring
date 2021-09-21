4306bf756037a70284372fa6ba2c12e784c45258
sinkyoungdeok <tlsrudejr123@nate.com>
2021-09-22 00:31:49 +0900

    [ADD] (쿼리 메소드 기능) 벌크성 수정 쿼리 완성

--- spring_data_jpa/4.query-method-function/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ spring_data_jpa/4.query-method-function/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -13,6 +13,8 @@ import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.entity.Team;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -29,6 +31,8 @@ class MemberRepositoryTest {
     MemberRepository memberRepository;
     @Autowired
     TeamRepository teamRepository;
+    @PersistenceContext
+    EntityManager em;
 
     @Test
     public void testMember() {
@@ -194,4 +198,23 @@ class MemberRepositoryTest {
         assertThat(slice.hasNext()).isTrue();
     }
 
+    @Test
+    public void bulkUpdate() throws Exception {
+        //given
+        memberRepository.save(new Member("member1", 10));
+        memberRepository.save(new Member("member2", 19));
+        memberRepository.save(new Member("member3", 20));
+        memberRepository.save(new Member("member4", 21));
+        memberRepository.save(new Member("member5", 40));
+        //when
+        int resultCount = memberRepository.bulkAgePlus(20);
+//        em.clear();
+
+        List<Member> result = memberRepository.findByUsername("member5");
+        Member member5 = result.get(0);
+        System.out.println("member5 = " + member5);
+
+        //then
+        assertThat(resultCount).isEqualTo(3);
+    }
 }
