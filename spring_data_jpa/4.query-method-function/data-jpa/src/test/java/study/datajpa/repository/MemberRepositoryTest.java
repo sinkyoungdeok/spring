dd9d23450cdcdda15e1579614f8c6af604751e6c
sinkyoungdeok <tlsrudejr123@nate.com>
2021-09-22 00:59:08 +0900

    [ADD] (쿼리 메소드 기능) @EntityGraph 완성

--- spring_data_jpa/4.query-method-function/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ spring_data_jpa/4.query-method-function/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -217,4 +217,33 @@ class MemberRepositoryTest {
         //then
         assertThat(resultCount).isEqualTo(3);
     }
+
+    @Test
+    public void findMemberLazy() throws Exception {
+        //given
+        //member1 -> teamA
+        //member2 -> teamB
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        teamRepository.save(teamA);
+        teamRepository.save(teamB);
+        memberRepository.save(new Member("member1", 10, teamA));
+        memberRepository.save(new Member("member2", 20, teamB));
+        em.flush();
+        em.clear();
+        //when
+//        List<Member> members = memberRepository.findAll();
+//        List<Member> members = memberRepository.findMemberFetchJoin();
+        List<Member> members = memberRepository.findEntityGraphByUsername("member1");
+        //then
+        for (Member member : members) {
+            member.getTeam().getName();
+            System.out.println("member = " + member.getUsername());
+            System.out.println("member.getTeam().getClass() = " + member.getTeam().getClass());
+            System.out.println("member.getTeam().getName() = " + member.getTeam().getName());
+        }
+        
+    }
+
+
 }
