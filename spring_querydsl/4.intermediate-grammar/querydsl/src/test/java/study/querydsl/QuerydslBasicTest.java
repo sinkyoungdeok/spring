17a233815c3eac526048510b3649a69e3ffafd62
sinkyoungdeok <tlsrudejr123@nate.com>
2021-09-25 11:28:26 +0900

    [ADD] (중급 문법) 프로젝션과 결과 반환 - DTO 조회 완성

--- spring_querydsl/4.intermediate-grammar/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ spring_querydsl/4.intermediate-grammar/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -2,15 +2,20 @@ package study.querydsl;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.ExpressionUtils;
+import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.core.types.dsl.NumberExpression;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.dto.MemberDto;
+import study.querydsl.dto.UserDto;
 import study.querydsl.entity.Member;
 import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
@@ -484,8 +489,81 @@ public class QuerydslBasicTest {
                 });
     }
 
-    
+    /* 프로젝션 결과 반환 - DTO 조회 */
 
+    @Test
+    public void findDtoByJPQL() {
+        List<MemberDto> result = em.createQuery(
+                "select new study.querydsl.dto.MemberDto(m.username, m.age) " +
+                        "from Member m", MemberDto.class)
+                .getResultList();
+        result.stream().forEach(System.out::println);
+    }
+
+    @Test
+    public void findDtoBySetter() {
+        List<MemberDto> result = queryFactory
+                .select(Projections.bean(MemberDto.class,
+                        member.username,
+                        member.age))
+                .from(member)
+                .fetch();
+        result.stream().forEach(System.out::println);
+    }
+
+    @Test
+    public void findDtoByField() {
+        List<MemberDto> result = queryFactory
+                .select(Projections.fields(MemberDto.class,
+                        member.username,
+                        member.age))
+                .from(member)
+                .fetch();
+        result.stream().forEach(System.out::println);
+    }
+
+    @Test
+    public void findDtoByConstructor() {
+        List<MemberDto> result = queryFactory
+                .select(Projections.constructor(MemberDto.class,
+                        member.username,
+                        member.age))
+                .from(member)
+                .fetch();
+        result.stream().forEach(System.out::println);
+    }
+
+    @Test
+    public void findUserDto() {
+
+
+        List<UserDto> result = queryFactory
+                .select(Projections.fields(UserDto.class,
+                        member.username.as("name"),
+                        member.age))
+                .from(member)
+                .fetch();
+        result.stream().forEach(System.out::println);
+
+    }
+
+    @Test
+    public void findUserDtoSubQuery() {
+        QMember memberSub = new QMember("memberSub");
+
+
+        List<UserDto> result = queryFactory
+                .select(Projections.fields(UserDto.class,
+                        member.username.as("name"),
+                        ExpressionUtils.as(JPAExpressions
+                                .select(memberSub.age.max())
+                                .from(memberSub), "age")
+                        ))
+                .from(member)
+                .fetch();
+        result.stream().forEach(System.out::println);
+
+    }
 
 
 
--- spring_querydsl/README.md
+++ spring_querydsl/README.md
@@ -1236,6 +1236,107 @@ for (Tuple tuple : result) {
 
 <details> <summary> 2. 프로젝션과 결과 반환 - DTO 조회 </summary>
 
+### 프로젝션과 결과 반환 - DTO 조회
+
+
+### 순수 JPA에서 DTO 조회
+
+**MemberDto**
+```java
+package study.querydsl.dto;
+import lombok.Data;
+@Data
+public class MemberDto {
+ private String username;
+ private int age;
+ public MemberDto() {
+ }
+ public MemberDto(String username, int age) {
+ this.username = username;
+ this.age = age;
+ }
+}
+```
+
+**순수 JPA에서 DTO 조회 코드**
+```java
+List<MemberDto> result = em.createQuery(
+ "select new study.querydsl.dto.MemberDto(m.username, m.age) " +
+ "from Member m", MemberDto.class)
+ .getResultList();
+```
+- 순수 JPA에서 DTO를 조회할 때는 new 명령어를 사용해야 함
+- DTO의 package이름을 다 적어줘야해서 지저분함
+- 생성자 방식만 지원함
+
+### Querydsl 빈 생성(Bean population)
+- 결과를 DTO 반환할 때 사용
+- 다음 3가지 방법 지원
+    - 프로퍼티 접근
+    - 필드 직접 접근
+    - 생성자 사용
+
+**프로퍼티 접근 - Setter**
+```java
+List<MemberDto> result = queryFactory
+ .select(Projections.bean(MemberDto.class,
+ member.username,
+ member.age))
+ .from(member)
+ .fetch();
+```
+
+**필드 직접 접근**
+```java
+List<MemberDto> result = queryFactory
+ .select(Projections.fields(MemberDto.class,
+ member.username,
+ member.age))
+ .from(member)
+ .fetch();
+```
+
+**별칭이 다를 때**
+```java
+package study.querydsl.dto;
+
+import lombok.Data;
+@Data
+public class UserDto {
+ private String name;
+ private int age;
+}
+```
+
+```java
+List<UserDto> fetch = queryFactory
+ .select(Projections.fields(UserDto.class,
+ member.username.as("name"),
+ ExpressionUtils.as(
+ JPAExpressions
+ .select(memberSub.age.max())
+ .from(memberSub), "age")
+ )
+ ).from(member)
+ .fetch();
+```
+- 프로퍼티나, 필드 접근 생성 방식에서 이름이 다를 때 해결 방안
+- `ExpressionUtils.as(source, alias)`: 필드나, 서브 쿼리에 별칭 사용
+- `username.as("memberName")`: 필드에 별칭 사용
+
+**생성자 사용**
+```java
+List<MemberDto> result = queryFactory
+ .select(Projections.constructor(MemberDto.class,
+ member.username,
+ member.age))
+ .from(member)
+ .fetch();
+}
+```
+
+
+
 </details>
 
 <details> <summary> 3. 프로젝션과 결과 반환 - @QueryProjection </summary>
