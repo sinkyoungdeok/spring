b32ced30f793ff1aa6d1719031e9be9c4e69b62c
sinkyoungdeok <tlsrudejr123@nate.com>
2021-09-26 12:54:56 +0900

    [ADD] (실무 활용 - 스프링 데이터 JPA와 Querydsl) 스프링 데이터 페이징 활용2 - CountQuery 최적화 완성

--- spring_querydsl/6.spring-data-jpa-vs-querydsl/querydsl/src/main/java/study/querydsl/repository/MemberRepositoryCustomImpl.java
+++ spring_querydsl/6.spring-data-jpa-vs-querydsl/querydsl/src/main/java/study/querydsl/repository/MemberRepositoryCustomImpl.java
@@ -2,13 +2,16 @@ package study.querydsl.repository;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.dto.QMemberTeamDto;
+import study.querydsl.entity.Member;
 
 import javax.persistence.EntityManager;
 import java.util.List;
@@ -96,7 +99,7 @@ public class MemberRepositoryCustomImpl implements MemberRepositoryCustom{
                 .limit(pageable.getPageSize())
                 .fetch();
 
-        long total = queryFactory
+        JPAQuery<Member> countQuery = queryFactory
                 .select(member)
                 .from(member)
                 .leftJoin(member.team, team)
@@ -105,11 +108,11 @@ public class MemberRepositoryCustomImpl implements MemberRepositoryCustom{
                         teamNameEq(condition.getTeamName()),
                         ageGoe(condition.getAgeGoe()),
                         ageLoe(condition.getAgeLoe())
-                )
-                .fetchCount();
+                );
 
 
-        return new PageImpl<>(content, pageable, total);
+        return PageableExecutionUtils.getPage(content, pageable, () -> countQuery.fetchCount());
+//        return new PageImpl<>(content, pageable, total);
     }
 
 
--- spring_querydsl/README.md
+++ spring_querydsl/README.md
@@ -2210,6 +2210,28 @@ Pageable pageable) {
 
 <details> <summary> 4. 스프링 데이터 페이지 활용2 - CountQuery 최적화 </summary>
 
+### 스프링 데이터 페이지 활용2 - CountQuery 최적화
+
+**PageableExecutionUtils.getPage()로 최적화**
+```java
+JPAQuery<Member> countQuery = queryFactory
+ .select(member)
+ .from(member)
+ .leftJoin(member.team, team)
+ .where(usernameEq(condition.getUsername()),
+ teamNameEq(condition.getTeamName()),
+ ageGoe(condition.getAgeGoe()),
+ ageLoe(condition.getAgeLoe()));
+// return new PageImpl<>(content, pageable, total);
+ return PageableExecutionUtils.getPage(content, pageable,
+countQuery::fetchCount);
+```
+- 스프링 데이터 라이브러리가 제공
+- count 쿼리가 생략 가능한 경우 생략해서 처리
+    - 페이지 시작이면서 컨텐츠 사이즈가 페이지 사이즈보다 작을 떄
+    - 마지막 페이지 일 때 (offset + 컨텐츠 사이즈를 더해서 전체 사이즈 구함)
+
+
 </details>
 
 <details> <summary> 5. 스프링 데이터 페이징 활용3 - 컨트롤러 개발 </summary>
