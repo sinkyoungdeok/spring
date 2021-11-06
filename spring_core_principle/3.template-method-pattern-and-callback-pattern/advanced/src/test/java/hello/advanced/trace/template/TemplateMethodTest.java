ce8d6309eb755ac7f892fd8f4835dd46862dca7d
sinkyoungdeok <tlsrudejr123@nate.com>
2021-11-06 12:16:20 +0900

    [ADD] (템플릿 메서드 패턴과 콜백 패턴) 템플릿 메서드 패턴 - 예제2 완성

--- spring_core_principle/3.template-method-pattern-and-callback-pattern/advanced/src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ spring_core_principle/3.template-method-pattern-and-callback-pattern/advanced/src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -1,4 +1,7 @@
 package hello.advanced.trace.template;
+import hello.advanced.trace.template.code.AbstractTemplate;
+import hello.advanced.trace.template.code.SubClassLogic1;
+import hello.advanced.trace.template.code.SubClassLogic2;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -27,4 +30,18 @@ public class TemplateMethodTest {
     long resultTime = endTime - startTime;
     log.info("resultTime={}", resultTime);
   }
+
+  /**
+   * 템플릿 메서드 패턴 적용
+   */
+  @Test
+  void templateMethodV1() {
+    AbstractTemplate template1 = new SubClassLogic1();
+    template1.execute();
+
+    AbstractTemplate template2 = new SubClassLogic2();
+    template2.execute();
+  }
+
+
 }
