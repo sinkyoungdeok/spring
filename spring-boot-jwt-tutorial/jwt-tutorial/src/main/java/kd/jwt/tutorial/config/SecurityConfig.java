e302eaa60744bf0aa4b8259efcce82c3f06a545c
sinkyoungdeok <tlsrudejr123@nate.com>
2021-12-02 18:49:39 +0900

    [ADD] 스프링 부트 버전이 높아짐에 따라 자동으로 body에 에러 메시지들을 입력해줬던 것이 사라져서 스프링 부트 버전을 낮춤, Cors안뜨게 설정

--- spring-boot-jwt-tutorial/jwt-tutorial/src/main/java/kd/jwt/tutorial/config/SecurityConfig.java
+++ spring-boot-jwt-tutorial/jwt-tutorial/src/main/java/kd/jwt/tutorial/config/SecurityConfig.java
@@ -15,20 +15,24 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.CorsFilter;
 
 @EnableWebSecurity // 기본적인 Web 보안을 활성화
 @EnableGlobalMethodSecurity(prePostEnabled = true) // @PreAuthorize 어노테이션을 메소드단위로 추가하기위해서 사용
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
   private final TokenProvider tokenProvider;
+  private final CorsFilter corsFilter;
   private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
   private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
   public SecurityConfig(
       TokenProvider tokenProvider,
+      CorsFilter corsFilter,
       JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
       JwtAccessDeniedHandler jwtAccessDeniedHandler
   ) {
     this.tokenProvider = tokenProvider;
+    this.corsFilter = corsFilter;
     this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
     this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
   }
@@ -54,6 +58,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // token을 사용하는 방식이기 때문에 csrf를 disable합니다.
         .csrf().disable()
 
+        .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+
         .exceptionHandling()
         .authenticationEntryPoint(jwtAuthenticationEntryPoint)
         .accessDeniedHandler(jwtAccessDeniedHandler)
@@ -74,6 +80,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .antMatchers("/api/hello").permitAll() // "/api/hello"에 대한 요청은 인증없이 접근을 허용
         .antMatchers("/api/authenticate").permitAll()
         .antMatchers("/api/signup").permitAll()
+
         .anyRequest().authenticated() // 나머지 요청들은 인증을 받아야됨
 
         .and()
