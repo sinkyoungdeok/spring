5b7fd7ba0618fc92faa64fac17e7879de8555833
sinkyoungdeok <tlsrudejr123@nate.com>
2021-12-21 23:23:05 +0900

    [ADD] (REST-API) PUT과 ObjectMapper

---
+++ kotlin-spring-mvc/mvc/src/main/kotlin/com/example/mvc/controller/put/PutApiController.kt
@@ -0,0 +1,56 @@
+package com.example.mvc.controller.put
+
+import com.example.mvc.model.http.Result
+import com.example.mvc.model.http.UserRequest
+import com.example.mvc.model.http.UserResponse
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api")
+class PutApiController {
+
+    @PutMapping("/put-mapping")
+    fun putMapping(): String{
+        return "put-mapping"
+    }
+
+    @RequestMapping(method = [RequestMethod.PUT], path = ["/request-mapping"])
+    fun requestMapping(): String {
+        return "request-mapping - put method"
+    }
+
+    @PutMapping("/put-mapping/object")
+    fun putMappingObject(@RequestBody userRequest: UserRequest) : UserResponse {
+        return UserResponse().apply{
+            this.result = Result().apply {
+                this.resultCode = "OK"
+                this.resultMessage = "성공"
+            }
+        }.apply {
+            this.description = "~~~~"
+
+        }.apply {
+            var userList = mutableListOf<UserRequest>()
+
+            userList.add(userRequest)
+
+            userList.add(UserRequest().apply {
+                this.name = "a"
+                this.age = 10
+                this.email = "a@gmail.com"
+                this.address = "a address"
+                this.phoneNumber = "010-1111-aaaa"
+            })
+            userList.add(UserRequest().apply {
+                this.name = "b"
+                this.age = 20
+                this.email = "b@gmail.com"
+                this.address = "b address"
+                this.phoneNumber = "010-1111-bbbb"
+            })
+
+            this.userRequest = userList
+        }
+
+    }
+}
--- kotlin-spring-mvc/mvc/src/main/kotlin/com/example/mvc/model/http/UserRequest.kt
+++ kotlin-spring-mvc/mvc/src/main/kotlin/com/example/mvc/model/http/UserRequest.kt
@@ -1,10 +1,6 @@
 package com.example.mvc.model.http
 
-import com.fasterxml.jackson.annotation.JsonProperty
-import com.fasterxml.jackson.databind.PropertyNamingStrategy
-import com.fasterxml.jackson.databind.annotation.JsonNaming
 
-@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy::class)
 data class UserRequest(
     var name:String?=null,
     var age:Int?=null,
