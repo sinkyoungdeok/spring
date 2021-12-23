31d95d26ae1a4576e0dcc72fd3e31bd1636dcce4
sinkyoungdeok <tlsrudejr123@nate.com>
2021-12-23 19:54:10 +0900

    [ADD] (실습!! TODO LIST WebApplication 만들기) Memory Database 설계와 JUnit으로 테스트하기

---
+++ kotlin-spring-mvc/todo/src/main/kotlin/com/example/todo/database/Todo.kt
@@ -0,0 +1,13 @@
+package com.example.todo.database
+
+import java.time.LocalDateTime
+
+data class Todo(
+    var index: Int?= null,
+    var title: String?=null,
+    var name: String?= null,
+    var description: String?= null,
+    var schedule: LocalDateTime?=null,
+    var createdAt: LocalDateTime?=null,
+    var updatedAt: LocalDateTime?=null
+)
