9db1a922d2cbc802bf0c923fc446c289597226ef
sinkyoungdeok <tlsrudejr123@nate.com>
2021-12-23 22:07:04 +0900

    [ADD] (실습!! TODO LIST WebApplication 만들기) Swagger 추가하기

--- kotlin-spring-mvc/todo/build.gradle.kts
+++ kotlin-spring-mvc/todo/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.6.2"
+	id("org.springframework.boot") version "2.5.6"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	kotlin("jvm") version "1.6.10"
 	kotlin("plugin.spring") version "1.6.10"
@@ -21,6 +21,9 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	implementation("io.springfox:springfox-swagger-ui:2.9.2")
+	implementation("io.springfox:springfox-swagger2:2.9.2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
--- kotlin-spring-mvc/todo/src/main/kotlin/com/example/todo/model/http/TodoDto.kt
+++ kotlin-spring-mvc/todo/src/main/kotlin/com/example/todo/model/http/TodoDto.kt
@@ -1,22 +1,53 @@
 package com.example.todo.model.http
 
 import com.example.todo.database.Todo
+import io.swagger.annotations.ApiModelProperty
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import javax.validation.constraints.AssertTrue
 import javax.validation.constraints.NotBlank
 
 data class TodoDto (
+    @field:ApiModelProperty(
+        value = "DB INDEX",
+        example = "1",
+        required = false
+    )
     var index:Int?=null,
 
+    @field:ApiModelProperty(
+        value = "일정명",
+        example = "일정관리",
+        required = true
+    )
     @field:NotBlank
     var title:String?=null,
 
+    @field:ApiModelProperty(
+        value = "일정설명",
+        example = "13시 스타벅스",
+        required = false
+    )
     var description:String ?=null,
 
+    @field:ApiModelProperty(
+        value = "시간",
+        example = "2020-01-01 00:00:00",
+        required = true
+    )
     @field:NotBlank
     var schedule: String?=null,
+
+    @field:ApiModelProperty(
+        value = "생성일자",
+        required = false
+    )
     var createdAt: LocalDateTime?=null,
+
+    @field:ApiModelProperty(
+        value = "수정일자",
+        required = false
+    )
     var updatedAt: LocalDateTime?=null
 ){
     @AssertTrue(message = "yyyy-MM-dd HH:mm:ss 포맷이 맞지 않습니다. ")
