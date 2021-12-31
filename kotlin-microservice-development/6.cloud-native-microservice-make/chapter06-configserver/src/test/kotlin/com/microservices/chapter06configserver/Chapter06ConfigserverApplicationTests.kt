ce2197e2ee73ed37801fb67857f9566cfc1348f3
sinkyoungdeok <tlsrudejr123@nate.com>
2022-01-01 00:55:20 +0900

    [ADD] (코틀린 마이크로서비스 개발) 클라우드 네이티브 마이크로서비스 만들기 - 291쪽까지 (컨피그 서버 만들기)

---
+++ kotlin-microservice-development/6.cloud-native-microservice-make/chapter06-configserver/src/test/kotlin/com/microservices/chapter06configserver/Chapter06ConfigserverApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.microservices.chapter06configserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Chapter06ConfigserverApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
