fc37369b83fc910d3dd00e027e5e45a64a925777
sinkyoungdeok <tlsrudejr123@nate.com>
2022-01-01 01:44:02 +0900

    [ADD] (코틀린 마이크로서비스 개발) 클라우드 네이티브 마이크로서비스 만들기 - 297쪽까지 (데이터 암호화)

--- kotlin-microservice-development/6.cloud-native-microservice-make/chapter06-configserver/build.gradle.kts
+++ kotlin-microservice-development/6.cloud-native-microservice-make/chapter06-configserver/build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springframework.cloud:spring-cloud-config-server")
+	implementation("org.springframework.cloud:spring-cloud-starter-bootstrap")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
