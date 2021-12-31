8d77cc68ef5fc7e5a556308cacb312ca02062815
sinkyoungdeok <tlsrudejr123@nate.com>
2021-12-31 22:28:47 +0900

    [ADD] (코틀린 마이크로 서비스 개발) 리액티브 스프링 데이터 - 257쪽까지(ReactiveMongoTemplate 사용)

--- kotlin-microservice-development/5.reactive-spring-data/chapter5/src/main/kotlin/com/microservices/chapter5/CustomerRepository.kt
+++ kotlin-microservice-development/5.reactive-spring-data/chapter5/src/main/kotlin/com/microservices/chapter5/CustomerRepository.kt
@@ -1,5 +1,21 @@
 package com.microservices.chapter5
 
-import org.springframework.data.repository.reactive.ReactiveCrudRepository
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate
+import org.springframework.stereotype.Repository
+import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toMono
+import javax.annotation.PostConstruct
 
-interface CustomerRepository: ReactiveCrudRepository<Customer, Int>
+@Repository
+class CustomerRepository(private val template: ReactiveMongoTemplate) {
+    companion object {
+        val initialCustomers = listOf(Customer(1, "Kotlin"),
+        Customer(2, "Spring"),
+        Customer(3, "Microservice", Customer.Telephone("+44", "7123456789")))
+    }
+
+    @PostConstruct
+    fun initializeRepository() = initialCustomers.map(Customer::toMono).map(this::create).map(Mono<Customer>::subscribe)
+
+    fun create(customer: Mono<Customer>) = template.save(customer)
+}
