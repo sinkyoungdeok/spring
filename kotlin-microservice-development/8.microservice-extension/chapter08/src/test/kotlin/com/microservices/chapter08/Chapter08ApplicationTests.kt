aaf6b7ada291dc1d0dac87fced784b088e58f6d4
sinkyoungdeok <tlsrudejr123@nate.com>
2022-01-01 22:43:16 +0900

    [ADD] (코틀린 마이크로서비스 개발) 마이크로서비스 확장 - 349쪽까지 (마이크로서비스 만들기)

---
+++ kotlin-microservice-development/8.microservice-extension/chapter08/src/test/kotlin/com/microservices/chapter08/Chapter08ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.microservices.chapter08
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Chapter08ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
