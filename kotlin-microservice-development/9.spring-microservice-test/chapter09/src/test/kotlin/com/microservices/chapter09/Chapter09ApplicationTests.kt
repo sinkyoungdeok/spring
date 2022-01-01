02a9cb1fc173119ce260ea441dfff227ecbbcc64
sinkyoungdeok <tlsrudejr123@nate.com>
2022-01-01 23:23:25 +0900

    [ADD] (코틀린 마이크로서비스 개발) 스프링 마이크로서비스 테스트 - 361쪽까지 (간단한 마이크로 서비스 프로젝트 세팅)

---
+++ kotlin-microservice-development/9.spring-microservice-test/chapter09/src/test/kotlin/com/microservices/chapter09/Chapter09ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.microservices.chapter09
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Chapter09ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
