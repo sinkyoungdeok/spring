9e4505ae39de9d887acf0fac854049f41f2f098e
sinkyoungdeok <tlsrudejr123@nate.com>
2022-01-02 11:45:11 +0900

    [ADD] (코틀린 마이크로서비스 개발) 마이크로서비스 모니터링 - 408쪽 까지 (장애 발생 마이크로서비스 프로젝트 세팅)

---
+++ kotlin-microservice-development/10.microservice-monitoring/chapter10/src/test/kotlin/com/microservices/chapter10/Chapter10ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.microservices.chapter10
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Chapter10ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
