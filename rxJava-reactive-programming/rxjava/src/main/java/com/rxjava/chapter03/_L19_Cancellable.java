db21228b0031307e215e44fd46bfa37d9e701437
sinkyoungdeok <tlsrudejr123@nate.com>
2022-01-07 04:36:38 +0900

    [ADD] (RxJava 리액티브 프로그래밍) 162쪽 - 리소스를 해제하는 방법 3가지

---
+++ rxJava-reactive-programming/rxjava/src/main/java/com/rxjava/chapter03/_L19_Cancellable.java
@@ -0,0 +1,6 @@
+package com.rxjava.chapter03;
+
+public interface _L19_Cancellable {
+
+  void cancel() throws Exception;
+}
---
+++ rxJava-reactive-programming/rxjava/src/main/java/com/rxjava/chapter03/_L21_Disposable.java
@@ -0,0 +1,9 @@
+package com.rxjava.chapter03;
+
+public interface _L21_Disposable {
+
+  void dispose();
+
+  boolean isDisposed();
+
+}
