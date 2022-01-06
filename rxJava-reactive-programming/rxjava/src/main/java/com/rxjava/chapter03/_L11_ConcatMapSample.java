c67114c3a4c7c0f24cfc0e752020ffbb70d72113
sinkyoungdeok <tlsrudejr123@nate.com>
2022-01-06 22:03:29 +0900

    [ADD] (RxJava 리액티브 프로그래밍) 146쪽 - concatMap 메서드 내에서 서로 다른 스레드로 작동하는 Flowable을 생성하는 예제

---
+++ rxJava-reactive-programming/rxjava/src/main/java/com/rxjava/chapter03/_L11_ConcatMapSample.java
@@ -0,0 +1,26 @@
+package com.rxjava.chapter03;
+
+import io.reactivex.Flowable;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
+
+public class _L11_ConcatMapSample {
+
+  public static void main(String[] args) throws InterruptedException {
+    Flowable<String> flowable =
+        Flowable.just("A", "B", "C")
+            .concatMap(data -> Flowable.just(data).delay(1000L, TimeUnit.MILLISECONDS));
+
+    flowable.subscribe(data -> {
+      String threadName = Thread.currentThread().getName();
+      String time =
+          LocalTime.now().format(DateTimeFormatter.ofPattern("ss.SSS"));
+      System.out.println(threadName + ": data=" + data + ", time=" + time);
+
+    });
+
+    Thread.sleep(4000L);
+  }
+
+}
