3597d04af733168a51468d3c8e414fd13990bb43
sinkyoungdeok <tlsrudejr123@nate.com>
2022-01-07 20:45:08 +0900

    [ADD] (RxJava 리액티브 프로그래밍) 219쪽 - concatMap(mapper) 예제

---
+++ rxJava-reactive-programming/rxjava/src/main/java/com/rxjava/chapter04/_L26_ConcatMapSample.java
@@ -0,0 +1,22 @@
+package com.rxjava.chapter04;
+
+import io.reactivex.Flowable;
+import java.util.concurrent.TimeUnit;
+
+public class _L26_ConcatMapSample {
+
+  public static void main(String[] args) throws InterruptedException {
+    Flowable<String> flowable = Flowable.range(10,3)
+        .concatMap(
+            sourceData -> Flowable.interval(500L, TimeUnit.MILLISECONDS)
+                .take(2)
+                .map(data -> {
+                  long time = System.currentTimeMillis();
+                  return time + "ms: [" + sourceData + "] " + data;
+                }));
+    flowable.subscribe(new _L01_DebugSubscriber<>());
+
+    Thread.sleep(4000L);
+  }
+
+}
