fc155817f7ca89aec57ed2fd9e2a6764ba062587
sinkyoungdeok <tlsrudejr123@nate.com>
2022-01-08 10:52:42 +0900

    [ADD] (RxJava 리액티브 프로그래밍) 305쪽 - combineLatest(source1, source2, combiner) 예제

---
+++ rxJava-reactive-programming/rxjava/src/main/java/com/rxjava/chapter04/_L83_CombineLatesSample1.java
@@ -0,0 +1,30 @@
+package com.rxjava.chapter04;
+
+import io.reactivex.Flowable;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class _L83_CombineLatesSample1 {
+
+  public static void main(String[] args) throws InterruptedException {
+    Flowable<Long> flowable1 =
+        Flowable.interval(300L, TimeUnit.MILLISECONDS)
+            .take(5);
+
+    Flowable<Long> flowable2 =
+        Flowable.interval(500L, TimeUnit.MILLISECONDS)
+            .take(3)
+            .map(data -> data + 100L);
+
+    Flowable<List<Long>> result = Flowable.combineLatest(
+        flowable1,
+        flowable2,
+        (data1, data2) -> Arrays.asList(data1, data2));
+
+    result.subscribe(new _L01_DebugSubscriber<>());
+
+    Thread.sleep(2000L);
+  }
+
+}
