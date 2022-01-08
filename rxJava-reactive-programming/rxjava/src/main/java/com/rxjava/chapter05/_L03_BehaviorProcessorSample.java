51dcde29262f0a8f676f52a81520d3fc887a8f72
sinkyoungdeok <tlsrudejr123@nate.com>
2022-01-08 14:59:11 +0900

    [ADD] (RxJava 리액티브 프로그래밍) 362쪽 - BehaviorProcessor 예제

---
+++ rxJava-reactive-programming/rxjava/src/main/java/com/rxjava/chapter05/_L03_BehaviorProcessorSample.java
@@ -0,0 +1,31 @@
+package com.rxjava.chapter05;
+
+import com.rxjava.chapter04._L01_DebugSubscriber;
+import io.reactivex.processors.BehaviorProcessor;
+
+public class _L03_BehaviorProcessorSample {
+
+  public static void main(String[] args) {
+    BehaviorProcessor<Integer> processor = BehaviorProcessor.create();
+
+    processor.subscribe(new _L01_DebugSubscriber<>("No.1"));
+
+    processor.onNext(1);
+    processor.onNext(2);
+    processor.onNext(3);
+
+    System.out.println("Subscriber No2.추가");
+    processor.subscribe(new _L01_DebugSubscriber<>("--- No.2"));
+
+    processor.onNext(4);
+    processor.onNext(5);
+
+    processor.onComplete();
+
+    System.out.println("Subscriber No.3 추가");
+    processor.subscribe(new _L01_DebugSubscriber<>("------ No.3"));
+
+
+  }
+
+}
