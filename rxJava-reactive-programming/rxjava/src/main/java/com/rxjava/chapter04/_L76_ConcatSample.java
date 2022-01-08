d183dd4647046f56396a3f35a56c27fd0745c4d0
sinkyoungdeok <tlsrudejr123@nate.com>
2022-01-08 10:36:19 +0900

    [ADD] (RxJava 리액티브 프로그래밍) 290쪽 - concat(source1, source2) 예제

---
+++ rxJava-reactive-programming/rxjava/src/main/java/com/rxjava/chapter04/_L76_ConcatSample.java
@@ -0,0 +1,25 @@
+package com.rxjava.chapter04;
+
+import io.reactivex.Flowable;
+import java.util.concurrent.TimeUnit;
+
+public class _L76_ConcatSample {
+
+  public static void main(String[] args) throws InterruptedException {
+    Flowable<Long> flowable1 =
+        Flowable.interval(300L, TimeUnit.MILLISECONDS)
+            .take(5);
+
+    Flowable<Long> flowable2 =
+        Flowable.interval(500L, TimeUnit.MILLISECONDS)
+            .take(2)
+            .map(data -> data + 100L);
+
+    Flowable<Long> result = Flowable.concat(flowable1, flowable2);
+
+    result.subscribe(new _L01_DebugSubscriber<>());
+
+    Thread.sleep(3000L);
+  }
+
+}
