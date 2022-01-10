d3226591186c2a0bbb5b149fa04dcac8872cb8c0
sinkyoungdeok <tlsrudejr123@nate.com>
2022-01-10 13:36:53 +0900

    [ADD] (스프링 시작하기) 2.4 뷰 컨트롤러로 작업하기

--- spring-in-action/taco-cloud/src/test/java/sla/tacocloud/HomeControllerTest.java
+++ spring-in-action/taco-cloud/src/test/java/sla/tacocloud/HomeControllerTest.java
@@ -11,7 +11,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@WebMvcTest(HomeController.class)
+@WebMvcTest
 class HomeControllerTest {
 
   @Autowired
