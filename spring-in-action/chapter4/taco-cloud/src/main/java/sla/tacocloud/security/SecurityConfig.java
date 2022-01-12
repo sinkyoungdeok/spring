b67fe3bd7110bee5dff54ff9d15be99a91ea723d
sinkyoungdeok <tlsrudejr123@nate.com>
2022-01-12 21:45:56 +0900

    [ADD] (스프링 시큐리티) 4.2 스프링 시큐리티 구성하기

--- spring-in-action/chapter4/taco-cloud/src/main/java/sla/tacocloud/security/SecurityConfig.java
+++ spring-in-action/chapter4/taco-cloud/src/main/java/sla/tacocloud/security/SecurityConfig.java
@@ -1,12 +1,27 @@
 package sla.tacocloud.security;
 
+import javax.sql.DataSource;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+  private final DataSource dataSource;
+  private final UserDetailsService userDetailsService;
+
+  @Bean
+  public PasswordEncoder encoder() {
+    return new BCryptPasswordEncoder();
+  }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
@@ -21,14 +36,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   public void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.inMemoryAuthentication()
-        .withUser("user1")
-        .password("{noop}password1")
-        .authorities("ROLE_USER")
-        .and()
-        .withUser("user2")
-        .password("{noop}password2")
-        .authorities("ROLE_USER");
+    auth
+        .userDetailsService(userDetailsService)
+        .passwordEncoder(encoder());
   }
 
 }
