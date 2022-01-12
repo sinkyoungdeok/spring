07b1d23adae4a6e1d97c51f801da2d44dd3c19d9
sinkyoungdeok <tlsrudejr123@nate.com>
2022-01-12 22:56:59 +0900

    [ADD] (스프링 시큐리티) 4.3 웹 요청 보안 처리하기

--- spring-in-action/chapter4/taco-cloud/src/main/java/sla/tacocloud/security/SecurityConfig.java
+++ spring-in-action/chapter4/taco-cloud/src/main/java/sla/tacocloud/security/SecurityConfig.java
@@ -28,10 +28,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     http
         .authorizeRequests()
         .antMatchers("/design", "/orders")
-        .access("hasRole('ROLE_USER')")
-        .antMatchers("/", "/**").access("permitAll")
+        .hasRole("ROLE_USER")
+        .antMatchers("/", "/**").permitAll()
         .and()
-          .httpBasic();
+        .formLogin()
+        .loginPage("/login")
+        .and()
+        .logout()
+        .logoutSuccessUrl("/")
+        .and()
+        .csrf();
   }
 
   @Override
