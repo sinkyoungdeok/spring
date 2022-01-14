b6d939eb5f694a8263531bef4df65bf6c4b25ff8
sinkyoungdeok <tlsrudejr123@nate.com>
2022-01-14 13:17:55 +0900

    [ADD] (6. REST 서비스 생성하기) 6.1 REST 컨트롤러 작성하기

--- spring-in-action/chapter6/tacocloud-api/src/main/java/tacos/web/api/DesignTacoController.java
+++ spring-in-action/chapter6/tacocloud-api/src/main/java/tacos/web/api/DesignTacoController.java
@@ -8,18 +8,22 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.hateoas.EntityLinks;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 //end::recents[]
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 //tag::recents[]
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import tacos.Order;
 import tacos.Taco;
+import tacos.data.OrderRepository;
 import tacos.data.TacoRepository;
 
 @RestController
@@ -42,83 +46,23 @@ public class DesignTacoController {
             0, 12, Sort.by("createdAt").descending());
     return tacoRepo.findAll(page).getContent();
   }
-  //end::recents[]
 
-//  @GetMapping("/recenth")
-//  public Resources<TacoResource> recentTacosH() {
-//    PageRequest page = PageRequest.of(
-//            0, 12, Sort.by("createdAt").descending());
-//    List<Taco> tacos = tacoRepo.findAll(page).getContent();
-//    
-//    List<TacoResource> tacoResources = 
-//        new TacoResourceAssembler().toResources(tacos);
-//    Resources<TacoResource> recentResources = 
-//        new Resources<TacoResource>(tacoResources);
-//    recentResources.add(
-//        linkTo(methodOn(DesignTacoController.class).recentTacos())
-//        .withRel("recents"));
-//    return recentResources;
-//  }
-
-  
-  
-//ControllerLinkBuilder.linkTo(DesignTacoController.class)
-//.slash("recent")
-//.withRel("recents"));
-
-  
-  
-  
-//  @GetMapping("/recenth")
-//  public Resources<TacoResource> recenthTacos() {
-//    PageRequest page = PageRequest.of(
-//            0, 12, Sort.by("createdAt").descending());
-//    List<Taco> tacos = tacoRepo.findAll(page).getContent();
-//
-//    List<TacoResource> tacoResources = new TacoResourceAssembler().toResources(tacos);
-//    
-//    Resources<TacoResource> tacosResources = new Resources<>(tacoResources);
-////    Link recentsLink = ControllerLinkBuilder
-////        .linkTo(DesignTacoController.class)
-////        .slash("recent")
-////        .withRel("recents");
-//
-//    Link recentsLink = 
-//        linkTo(methodOn(DesignTacoController.class).recentTacos())
-//        .withRel("recents");
-//    tacosResources.add(recentsLink);
-//    return tacosResources;
-//  }
-  
-  //tag::postTaco[]
   @PostMapping(consumes="application/json")
   @ResponseStatus(HttpStatus.CREATED)
   public Taco postTaco(@RequestBody Taco taco) {
     return tacoRepo.save(taco);
   }
-  //end::postTaco[]
   
   
   @GetMapping("/{id}")
-  public Taco tacoById(@PathVariable("id") Long id) {
+  public ResponseEntity<Taco> tacoById(@PathVariable("id") Long id) {
     Optional<Taco> optTaco = tacoRepo.findById(id);
     if (optTaco.isPresent()) {
-      return optTaco.get();
+      return new ResponseEntity<>(optTaco.get(), HttpStatus.OK);
     }
-    return null;
+    return new ResponseEntity<>(null, HttpStatus.OK);
   }
-  
-//  @GetMapping("/{id}")
-//  public ResponseEntity<Taco> tacoById(@PathVariable("id") Long id) {
-//    Optional<Taco> optTaco = tacoRepo.findById(id);
-//    if (optTaco.isPresent()) {
-//      return new ResponseEntity<>(optTaco.get(), HttpStatus.OK);
-//    }
-//    return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-//  }
 
-  
-//tag::recents[]
 }
-//end::recents[]
+
 
