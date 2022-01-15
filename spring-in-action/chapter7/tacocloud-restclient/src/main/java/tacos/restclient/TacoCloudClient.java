5bca4511468ac7046bd721503688276edb57d5e8
sinkyoungdeok <tlsrudejr123@nate.com>
2022-01-15 13:13:01 +0900

    [ADD] (7. REST 서비스 사용하기) 7.1 RestTemplate으로 REST 엔듶포인트 사용하기

--- spring-in-action/chapter7/tacocloud-restclient/src/main/java/tacos/restclient/TacoCloudClient.java
+++ spring-in-action/chapter7/tacocloud-restclient/src/main/java/tacos/restclient/TacoCloudClient.java
@@ -1,22 +1,93 @@
 package tacos.restclient;
 
+import java.net.URI;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.hateoas.Resources;
 import org.springframework.hateoas.client.Traverson;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.util.UriComponentsBuilder;
 import tacos.Ingredient;
 import tacos.Taco;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class TacoCloudClient {
 
+  private final RestTemplate restTemplate;
 
+  public Ingredient getIngredientById(String ingredientId) {
+    return restTemplate.getForObject("http://localhost:8080/ingredients/{id}",
+        Ingredient.class, ingredientId);
+  }
+
+  public Ingredient getIngredientById2(String ingredientId) {
+    Map<String, String> urlVariables = new HashMap<>();
+    urlVariables.put("id", ingredientId);
+    return restTemplate.getForObject("http://localhost:8080/ingredients/{id}",
+        Ingredient.class, urlVariables);
+  }
+
+  public Ingredient getIngredientById3(String ingredientId) {
+    Map<String, String> urlVariables = new HashMap<>();
+    urlVariables.put("id", ingredientId);
+    URI url = UriComponentsBuilder
+        .fromHttpUrl("http://localhost:8080/ingredients/{id}")
+        .build(urlVariables);
+    return restTemplate.getForObject(url, Ingredient.class);
+  }
+
+  public Ingredient getIngredientById4(String ingredientId) {
+    ResponseEntity<Ingredient> responseEntity =
+        restTemplate.getForEntity("http://localhost:8080/ingredients/{id}",
+            Ingredient.class, ingredientId);
+    log.info("Fetched time: " +
+        responseEntity.getHeaders().getDate());
+
+    return responseEntity.getBody();
+  }
+
+  public void updateIngredient(Ingredient ingredient) {
+    restTemplate.put("http://localhost:8080/ingredients/{id}",
+        ingredient,
+        ingredient.getId());
+  }
+
+  public void deleteIngredient(Ingredient ingredient) {
+    restTemplate.delete("http://localhost:8080/ingredients/{id}",
+        ingredient.getId());
+  }
+
+  public Ingredient createIngredient(Ingredient ingredient) {
+    return restTemplate.postForObject("http://localhost:8080/ingredients",
+        ingredient,
+        Ingredient.class);
+  }
+
+  public URI createIngredient2(Ingredient ingredient) {
+    return restTemplate.postForLocation("http://localhost:8080/ingredients", ingredient);
+  }
+
+  public Ingredient createIngredient3(Ingredient ingredient) {
+    ResponseEntity<Ingredient> responseEntity =
+        restTemplate.postForEntity("http://localhost:8080/ingredients",
+            ingredient,
+            Ingredient.class);
+
+    log.info("New resource created at " +
+        responseEntity.getHeaders().getLocation());
+
+    return responseEntity.getBody();
+  }
 }
