6182ab25c9e80809b88eb2f96f9fce6941946bf5
sinkyoungdeok <tlsrudejr123@nate.com>
2022-01-18 00:35:36 +0900

    [ADD] (10. 리액터 개요) 10.3.4 리액티브 타입에 로직 오퍼레이션 수행하기

--- spring-in-action/chapter10/reactor/src/test/java/chapter10/reactor/FluxBufferingTests.java
+++ spring-in-action/chapter10/reactor/src/test/java/chapter10/reactor/FluxBufferingTests.java
@@ -72,4 +72,37 @@ public class FluxBufferingTests {
         .verifyComplete();
   }
 
+  @Test
+  public void all() {
+    Flux<String> animalFlux = Flux.just(
+        "aardvark", "elephant", "koala", "eagle", "kangaroo");
+
+    Mono<Boolean> hasAMono = animalFlux.all(a -> a.contains("a"));
+    StepVerifier.create(hasAMono)
+        .expectNext(true)
+        .verifyComplete();
+
+    Mono<Boolean> hasKMono = animalFlux.all(a -> a.contains("k"));
+    StepVerifier.create(hasKMono)
+        .expectNext(false)
+        .verifyComplete();
+  }
+
+  @Test
+  public void any() {
+    Flux<String> animalFlux = Flux.just(
+        "aardvark", "elephant", "koala", "eagle", "kangaroo");
+
+    Mono<Boolean> hasAMono = animalFlux.any(a -> a.contains("a"));
+
+    StepVerifier.create(hasAMono)
+        .expectNext(true)
+        .verifyComplete();
+
+    Mono<Boolean> hasZMono = animalFlux.any(a -> a.contains("z"));
+    StepVerifier.create(hasZMono)
+        .expectNext(false)
+        .verifyComplete();
+  }
+
 }
